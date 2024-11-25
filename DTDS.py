b204de1f3e3f0214ce9a287d990b5921ccce4e92
kurtis <kurtisang@gmail.com>
2024-11-25 12:32:22 -0500

    Final Draft

--- DTDS.py
+++ DTDS.py
@@ -2,20 +2,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load data
-df = pd.read_csv('Data.csv')
 
-# Extract data
-time = np.array(df['Time'].values.tolist())
-cIV = np.array(df['Cocaine Intravenous (ng/mL)'].values.tolist())
+df = pd.read_csv('Data.csv')
+time_data = np.array(df['Time (h)'].values.tolist())
+cIV = np.array(df['Cocaine Intravenous (ng/mL)'].values.tolist())   
 cIN = np.array(df['Cocaine Intranasal (ng/mL)'].values.tolist())
 cSM = np.array(df['Cocaine Smoked (ng/mL)'].values.tolist())
 
+
 cIN_adjusted = cIN[np.argmax(cIN):]
-time_adjusted = time[np.argmax(cIN):]
+time_adjusted = time_data[np.argmax(cIN):]
+
 
 def cocaine_exp_analysis(time, cocaine_data, route_name):
-    # Reversal and Pre-Processing 
+    # Reversal was needed to have a correct line of best fit that is a decreasing function
     cocaine_rev = cocaine_data[::-1]
     cocaine2 = cocaine_rev[:-1]
 
@@ -25,39 +25,44 @@ def cocaine_exp_analysis(time, cocaine_data, route_name):
 
     # Updating Function based on Best Fit 
     def updating(x):
-        return (x - b) / a
+        return (x-b)/a
 
     # Updating concentrations with an initial value
     values = [max(cocaine_data)]  
-    terminal = np.linspace(0, 12, 400)
+    terminal = np.linspace(0, 12, 401)
     
     for i in range(len(terminal)):
         values.append(updating(values[i]))
 
-    # Graphs for analysis
-    diagonal = np.linspace(0, 250, 399)
-    
-    # Plotting figures
+
+    diagonal = np.linspace(0, 12, 400)
     fig1, ax1 = plt.subplots()
     fig2, ax2 = plt.subplots()
-    fig3, ax_rsum = plt.subplots(1, 3, figsize=(15, 5))
 
     # Plot diagonal and raw data with line of best fit
     ax1.set_title("Updating Function and Line of Best Fit")
     ax1.plot(diagonal, diagonal, label='Diagonal')
     ax1.scatter(cocaine_rev[:-1], cocaine_rev[1:], c='black', s=10, label=f'{route_name} Cocaine Raw Data Updating Function')
     ax1.plot(cocaine_rev[:-1], a * np.int64(cocaine_rev[:-1]) + b, label=str(line1), color='k', linestyle='dashed')
-    
+    ax1.legend()
+
     # Plot the solution from updating function
     ax2.set_title(f'{route_name} Solution')
     ax2.plot(terminal, values[:-1], c='k', label=f'{route_name} Solution')
     ax2.scatter(time, cocaine_data, label='Raw Data', c='k', s=50)
     ax2.legend()
-    ax1.legend()
 
-    # Trapezoidal Riemann Sum (AUC analysis)
+    plt.show()
+
+ # computing the Riemann Sum
+def rsum_trapezoid(time, cocaine_data,route_name):
+    return print(f'Riemann Sum for {route_name} Route of Administration: '+str(np.sum(np.diff(time) * ((cocaine_data[:-1] + cocaine_data[1:]) / 2))))
+
+
+def riemann_sum_analysis(time, cocaine_data, route_name):
+    fig3, ax_rsum = plt.subplots(1, 3, figsize=(15, 5))
     fig3.suptitle(f'{route_name} AUC Analysis')
-    
+
     # Left sum
     ax_rsum[0].set_title('Left Sum')
     time_left = time[:-1]
@@ -83,3 +88,11 @@ def cocaine_exp_analysis(time, cocaine_data, route_name):
     ax_rsum[2].bar(time_right, cocaine_right, width=0.03, alpha=0.5, align='edge', edgecolor='k')
 
     plt.show()
+    rsum_trapezoid(time,cocaine_data,route_name)
+
+
+def run(time,list,route_name):
+    cocaine_exp_analysis(time[np.argmax(list):],list[np.argmax(list):],route_name)
+    riemann_sum_analysis(time,list,route_name)
+    plt.show()
+
